ec1c44629be83342a4b292fb6c24352ae86fe256
Jilroge7 <1672@holbertonschool.com>
2021-06-03 23:40:03 +0000

    adding another partial iteration of stat

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,33 +3,39 @@
 Python script takes URL from stdin and compute exact metrics
 """
 import sys
-import re
 import traceback
 
 
 if __name__ == "__main__":
 
-    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     file_list = []
     stat_dict = {}
     line_num = 0
-    total = 0
-    stat_regex = '\s+\d+\s+'
-    file_regex = '\s+\d+\s+(.*)'
+    file_size = 0
+    line_regex = '.+ .+ \d+'
 
-    for line in sys.stdin:
-        if line == "":
-            continue
-        line_num += 1
-        total += sys.argv[5]
-        if line_num % 10 == 0:
-            line_num = 0
-            print("File size: {}".format(total))
-
-        for i in codes:
-            if i not in codes[i] or type(i) != int:
-                continue
-
-    
-    
-    print("{}: {}".format(sys.argv[4]))
+    for stat in codes:
+        stat_dict[stat] = 0
+    try:
+        for line in sys.stdin:
+            if line == "":
+                pass
+            file_list = line.split(" ")
+            flist_length = len(file_list)
+            if flist_length < 9:
+                pass
+            line_num += 1
+            file_size += int(file_list[-1])
+            if line_num % 10 == 0:
+                print("File size: {}".format(file_size))
+                line_num = 0
+            for key, value in stat_dict.items():
+                if stat_dict[key] == file_list[7]:
+                    value += 1
+                    print("{}: {}".format(key, value))
+    except(KeyboardInterrupt):
+        for line in sys.stdin:
+            if line_num % 10 == 0:
+                print("File size: {}".format(file_size))
+            line_num += 1
