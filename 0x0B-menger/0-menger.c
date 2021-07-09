2e316811c02ba66ec85a4c6d29721185357d43dc
Jilroge7 <1672@holbertonschool.com>
2021-07-09 05:03:21 +0000

    adding c

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "menger.h"
+
+/**
+* menger - function
+*
+* @level: level of menger sponge to draw
+*
+* Description: function that draws a 2D Menger Sponge
+* Return: nothing
+*/
+
+void menger(int level)
+{
+	if (level < 0)
+		return;
+}
