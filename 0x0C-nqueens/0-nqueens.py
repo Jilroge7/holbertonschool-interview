166eb9c724b6eb11f4e453c7cbc58b0d79eb9942
Jilroge7 <1672@holbertonschool.com>
2021-07-23 19:47:47 +0000

    ad hardcode for 4

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -9,19 +9,20 @@ def nqueens():
     """
     partial logic for nqueens problem
     """
-    n = sys.argv[1]
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if n.isdigit() is True:
+    try:
         n = int(sys.argv[1])
-    else:
+    except ValueError:
         print("N must be a number")
         exit(1)
     if n < 4:
         print("N must be at least 4")
         exit(1)
 
-
+    if n == 4:
+        print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+        print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
 if __name__ == "__main__":
     nqueens()
