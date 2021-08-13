f45e5ffc536488faf831e4eb1afca9a6fd1cd053
Jilroge7 <1672@holbertonschool.com>
2021-08-13 04:43:53 +0000

    new logic with promise

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,24 @@
 #!/usr/bin/node
+const process = require('process');
 const request = require('request');
-const reqURL = 'https://swapi-api.hbtn.io/api/films/';
 const id = process.argv[2];
+const reqURL = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(reqURL + id, function (error, result, body) {
-  const charList = JSON.parse(body).characters;
-  for (let i = 0; i < charList.length; i++) {
-    request(charList[i], function (error, result, body) {
-      console.log(JSON.parse(body).name);
-      if (error) {
-        return (console.log('error'));
-      }
-    });
-  }
-  if (error) {
-    return (console.log('error'));
+request(reqURL, async (error, response, body) => {
+  if (!error) {
+    const chars = JSON.parse(body).characters;
+    const newPromise = (reqURL) => {
+      return new Promise(function (resolve, reject) {
+        request(reqURL, (error, response, body) => {
+          if (error) reject(error);
+          else resolve(body);
+        });
+      });
+    };
+    for (const char in chars) {
+      const star = await newPromise(chars[char]);
+      const charList = JSON.parse(star);
+      console.log(charList.name);
+    }
   }
 });
