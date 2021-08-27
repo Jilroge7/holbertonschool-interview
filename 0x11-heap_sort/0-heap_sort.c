0a1c7cf20f87d781ccdaf65ad1d00717c6d5b93b
Jilroge7 <1672@holbertonschool.com>
2021-08-27 04:35:27 +0000

    adding incomplete task heap sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+
+/**
+* heap_sort - Sorts an array of integers with sift-down
+*
+* @array: The array to be printed
+* @size: Number of elements in @array
+* Return: Nothing
+*/
+void heap_sort(int *array, size_t size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+	    max(array, size);
+}
+
+/**
+* max - gets max number of array
+*
+* @array: The array to be printed
+* @size: Number of elements in @array
+* Return: Nothing
+*/
+int max(int *array, size_t size)
+{
+	int i;
+	int max = array[0];
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+
+/**
+* sift_down - Sorts an array of integers
+*
+* @array: The array to sift
+* @size: size of the array
+* Return: Nothing
+*/
+void sift_down(int *array, size_t size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+	    print_array(array, size);
+}
