7b42d84846fe2abb4567c8db9763d1ded20296d3
Jilroge7 <1672@holbertonschool.com>
2021-10-14 23:35:06 +0000

    betty fix

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -9,22 +9,22 @@
 */
 void merge_sort(int *array, size_t size)
 {
-    /*int left_sublist, right_sublist, merged_array;*/
-    int start, end, mid;
+/*int left_sublist, right_sublist, merged_array;*/
+	int start, end, mid;
 
-    if (size < 2 || array == NULL)
-        return;
+	if (size < 2 || array == NULL)
+		return;
 
-    start = array[0];
-    end = array[size -1];
+	start = array[0];
+	end = array[size - 1];
 
-    if (start < end)
-    {
-        mid = (start + end) / 2;
-    }
-    printf("Merging...\n");
-    /*printf("[left]: %d\n", left_sublist);*/
-    /*printf("[right]: %d\n", right_sublist);*/
-    printf(", \n");
-    printf("[Done]: %d\n", mid);
+	if (start < end)
+	{
+		mid = (start + end) / 2;
+	}
+	printf("Merging...\n");
+	/*printf("[left]: %d\n", left_sublist);*/
+	/*printf("[right]: %d\n", right_sublist);*/
+	printf(", \n");
+	printf("[Done]: %d\n", mid);
 }
