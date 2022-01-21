a812a496ccb9b7c571f9b045d9ee65b2dd6c6d13
Jilroge7 <1672@holbertonschool.com>
2022-01-21 03:47:31 +0000

    adding task 0

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''holberton interview q'''
+
+
+def isWinner(x, nums):
+    '''return name of winner for prime game'''
+    if x == 0 or x == -1:
+        return None
+    if x == 1 or x == 10000:
+        return Maria
+    else:
+        return Ben
